31fe5657e337b94b2503d25f20dbf79c2ae45293
elhss <elhamhosseini92@yahoo.com>
2019-12-22 17:16:08 +0330

    step2

--- index.php
+++ index.php
@@ -5,7 +5,23 @@
      My first php page
     </h1>
     <?php
-        echo "Hello world!";
+        $name = "khahani";
+        echo "Hello world!".$name;
+        echo "<br>";
+        echo "Hello world! $name";
+        echo "<br>";
+        echo $name;
+        $second = 4;
+        echo "<br>";
+        $first = 15;
+        echo "<br>";
+        echo $first + $second;
+        function write(){
+            $localvariable = 5;
+            echo $localvariable;
+        }
+        write();
+
     ?>
 </body>
 </html>
